c6aa5ae2d116009f80c5cd0ebfdff890349ad8b5
Renzo Mayer <rmayer@onpointglobal.com>
2021-03-16 13:03:51 -0300

    better names

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh -l
 
 branch=$1
-parent_path=$2
+composer_path=$2
 project_path=$3
 secret=$4
 composer_package_name=$(jq '.name' $project_path/composer.json)
 git config --global user.name github-actions
 git config --global user.email github-actions@github.com
-echo -n $1 >> token.txt
-gh auth login --with-token < token.txt
+echo -n $secret >> token.txt
+gh auth login --with-token < $secret
 rm token.txt
-cd $parent_path
-composer config --global github-oauth.github.com ${{ secrets.PAT }}
+cd $composer_path
+composer config --global github-oauth.github.com $secret
 composer update-$branch "$composer_package_name"
 git commit -am "${{ github.event.head_commit.message }} - update composer with $composer_package_name"
 git push origin $branch
