bfe1d61d59f57484f68532695ac0e0d88c4ea1ac
Renzo Mayer <rmayer@onpointglobal.com>
2021-03-16 13:21:24 -0300

    use specific composer version

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ branch=$1
 composer_parent_path=$2
 composer_project_path=$3
 secret=$4
+export COMPOSER=composer-$branch.json
 composer_package_name=$(jq '.name' $composer_project_path/composer.json)
 git config --global user.name github-actions
 git config --global user.email github-actions@github.com
@@ -12,6 +13,7 @@ gh auth login --with-token < $secret
 rm token.txt
 cd $composer_path
 composer config --global github-oauth.github.com $secret
-composer update-$branch "$composer_package_name"
+composer set-version
+composer update "$composer_package_name"
 git commit -am "${{ github.event.head_commit.message }} - update composer with $composer_package_name"
 git push origin $branch
