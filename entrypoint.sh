48edef8292a7765138b1247f2ef086f4734b5e63
Renzo Mayer <rmayer@onpointglobal.com>
2021-03-17 17:30:01 -0300

    keep debugging , why there is no change on composer lock

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,13 @@ composer_package_name=$(jq '.name' $composer_project_path/composer.json)
 git config --global user.name github-actions
 git config --global user.email github-actions@github.com
 echo -n $secret >> token.txt
-gh auth login --with-token $secret
+gh auth login --with-token < $secret
 rm token.txt
 composer config --global github-oauth.github.com $secret
 export COMPOSER=composer-$branch.json
 cd $composer_parent_path && composer update "$composer_package_name"
 pwd
+git config --get remote.origin.url
 git status
 git commit -am "update composer.lock with $composer_package_name"
 git push origin $branch
