f8954377dfad41e45763ce6fbf62424a3e515d1f
Renzo Mayer <rmayer@onpointglobal.com>
2021-03-17 13:15:41 -0300

    try to check what is going on in the fs

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,9 @@ git config --global user.email github-actions@github.com
 echo -n $secret >> token.txt
 gh auth login --with-token $secret
 rm token.txt
-cd notexistingfolder
 cd $composer_path
+cat composer.json
+ls
 composer config --global github-oauth.github.com $secret
 composer self-update 2.0.9
 export COMPOSER=composer-$branch.json
