6c2b7cc558c0fb531c53c89104262f0d21af8aa8
Renzo Mayer <rmayer@onpointglobal.com>
2021-03-17 17:36:27 -0300

    test without quotes, also put token from file instead of variable as is documented

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,11 @@ composer_package_name=$(jq '.name' $composer_project_path/composer.json)
 git config --global user.name github-actions
 git config --global user.email github-actions@github.com
 echo -n $secret >> token.txt
-gh auth login --with-token < $secret
+gh auth login --with-token < token.txt
 rm token.txt
 composer config --global github-oauth.github.com $secret
 export COMPOSER=composer-$branch.json
-cd $composer_parent_path && composer update "$composer_package_name"
+cd $composer_parent_path && composer update $composer_package_name
 pwd
 git config --get remote.origin.url
 git status
