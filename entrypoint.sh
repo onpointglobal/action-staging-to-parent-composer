28846e522998f69f2d794639ce2426301c882cad
Renzo Mayer <rmayer@onpointglobal.com>
2021-04-05 16:47:27 -0300

    update arg without space

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,10 @@ composer_project_path=$3
 secret=$4
 tag_version=$5
 composer_package_name=$(jq -r '.name' "$composer_project_path"/composer.json)
-echo "$composer_project_path"
-echo "$composer_package_name"
 update_arg="$composer_package_name"
 if [ -z "$tag_version" ]; then
     composer_version=$(echo $tag_version | cut -c 2-) 
-    update_arg = "$composer_package_name":"$composer_version"  
+    update_arg="$composer_package_name":"$composer_version"  
 fi
 
 export COMPOSER=composer-$branch.json
