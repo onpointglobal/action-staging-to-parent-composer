bcb675d1a8b9f6dc0c76d942ca4819e502658457
Renzo Mayer <rnzmayer@gmail.com>
2021-08-03 14:32:41 -0300

    upgade to make global auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ composer_parent_path=$2
 composer_project_path=$3
 secret=$4
 tag_version=$5
+composer_wpmudev=$6
 composer_package_name=$(jq -r '.name' "$composer_project_path"/composer.json)
 update_arg="$composer_package_name"
 if [ ! -z "${tag_version}" ]; then
@@ -21,6 +22,7 @@ pwd
 composer set-version
 export COMPOSER=composer-$branch.json
 composer config --global github-oauth.github.com $secret
+composer config --global http-basic.wpmudev.com $composer_wpmudev null
 composer require $update_arg
 
 git config --global user.name github-actions
