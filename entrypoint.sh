6d8d4af0bae5d65e01e3a3b9b7dd24a0473c33ad
Martin Vera <tinchovera8@hotmail.com>
2024-04-03 11:41:25 -0300

    Update V3 tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,14 @@ yoast_premium=$7
 composer_package_name=$(jq -r '.name' "$composer_project_path"/composer.json)
 update_arg="$composer_package_name"
 
+echo "branch value: $branch"
+echo "composer_parent_path value: $composer_parent_path"
+echo "composer_project_path value: $composer_project_path"
+echo "secret value: $secret"
+echo "tag_version value: $tag_version"
+echo "composer_wpmudev value: $composer_wpmudev"
+echo "yoast_premium value: $yoast_premium"
+
 echo "token1"
 echo "yoast_premium value: $yoast_premium"
 echo "token2"
