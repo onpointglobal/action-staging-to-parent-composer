ffcd7a704527842758c52fb6c8fd2b6f044f3a8b
Martin Vera <tinchovera8@hotmail.com>
2024-04-03 08:43:12 -0300

    FRMM-5172: Adding Yoast token only in bash 7

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,9 @@ tag_version=$5
 composer_wpmudev=$6
 composer_package_name=$(jq -r '.name' "$composer_project_path"/composer.json)
 update_arg="$composer_package_name"
-YOAST_PREMIUM=$7
+
 echo "token1"
-echo $YOAST_PREMIUM
+echo "$YOAST_PREMIUM"  # Access secret from environment variable
 echo "token2"
 
 if [ ! -z "${tag_version}" ]; then
